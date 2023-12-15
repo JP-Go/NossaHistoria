919525865079b50b4167da121c3f86dad28ae65b
João Pedro <jpedro1580@gmail.com>
2023-12-14 21:38:07 -0300

    Create a timeline component

---
+++ src/components/Timeline/Timeline.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TimelineItem from './TimelineItem.vue';
+</script>
+
+<template>
+  <div class="container">
+    <div class="relative flex flex-col items-center gap-24 py-8">
+      <div class="bg-sky-900 h-full md:w-2 w-1 z-0 absolute" />
+      <TimelineItem reverse />
+      <TimelineItem />
+      <TimelineItem reverse />
+      <TimelineItem />
+    </div>
+  </div>
+</template>
---
+++ src/components/Timeline/TimelineItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  reverse?: boolean;
+}>();
+</script>
+<template>
+  <div
+    class="grid grid-cols-[1fr_clamp(40px,20%,300px)_1fr] place-items-center z-0 relative px-4 sm:px-0"
+    :style="{ direction: reverse ? 'rtl' : 'ltr' }"
+  >
+    <img
+      src="https://picsum.photos/400/400"
+      alt="Image"
+      class="rounded-md border-4 md:border-8 border-sky-900 lg:w-auto md:w-64 w-full aspect-square justify-self-end"
+    />
+    <div class="flex items-center justify-center relative w-full">
+      <div class="w-4 h-4 md:w-6 md:h-6 rounded-full bg-sky-900 absolute" />
+      <div class="w-full h-1 md:h-2 bg-sky-900 place-self-center -z-10" />
+    </div>
+    <div
+      class="rounded-md border-4 md:border-8 border-sky-900 p-2 bg-white w-7/8 sm:max-w-[400px] justify-self-start"
+    >
+      <h2 class="font-bold md:text-3xl mb-2 ltr">The title on it (2023-01)</h2>
+      <p class="font-light text-sm md:text-md ltr">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+        aspernatur porro ea at minima, nostrum, modi deserunt iusto placeat
+        molestiae iure. Aliquid mollitia error veniam inventore, animi obcaecati
+        amet culpa?
+      </p>
+    </div>
+  </div>
+</template>
