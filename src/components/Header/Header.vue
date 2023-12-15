7acb5857532e8213206dbaf0d3f13466adaa4921
João Pedro <jpedro1580@gmail.com>
2023-12-14 21:37:55 -0300

    Add small shadow on the header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-screen position-fixed bg-white font-serif">
-    Laiza e Eu
+  <header class="w-screen fixed bg-white px-8 py-4 z-20 shadow-lg">
+    <p class="text-3xl">Laiza e Eu</p>
   </header>
 </template>
